52b332a23643e054dfc63410fbfbf3970cdf5d14
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-04-15 12:21:11 -0300

    add- teste fundo

---
+++ script.js
@@ -0,0 +1,13 @@
+function iniciarValidacao(){
+    const form = document.queryselector('.form');
+    const nomeInput = document.getElementById('nomeCompleto');
+    const erroNome = document.getElementById('nomeCompletoError');
+
+    form.addEventListener('submit', function(e){
+    
+
+
+
+    }
+
+}
